070edd19dbfb7f18bccbaf7a9f41061356f4e6ef
Jessica Ouyang <ouyangj@stanford.edu>
2017-06-04 22:42:48 -0700

    mod stuff

--- modulo.js
+++ modulo.js
@@ -6,30 +6,47 @@ diffieHellmanApp.controller('ModuloController', ['$scope', function($scope) {
    // We defined an object called 'main' with a single property 'title' that is used
    // by the html view template to get the page's title in the browser tab.
     $scope.guess1 = '';
-    $scope.answer1 = '';
+    $scope.answer1 = 'XX';
 
     $scope.checkAnswer1 = function() {
-        if($scope.guess1.length === 0) {
-            $scope.answer1 = '';
-        } else if($scope.guess1 === '4') {
-            $scope.answer1 = 'Correct!';
+        if($scope.guess1 === '4') {
+            $scope.answer1 = 'CORRECT';
+            $('#answer1').css('background-color','#b8efa2');
+            $('#answer1').css('color','#75aa5f');
+            $('#answer1').css('border','1px solid #75aa5f');
         } else {
-            $scope.answer1 = 'That\'s not it. Try again!';
+            $scope.answer1 = 'INCORRECT';
+            $('#answer1').css('background-color','#fc9c9c');
+            $('#answer1').css('color','#ce4848');
+            $('#answer1').css('border','1px solid #ce4848');
+            $('#answer1').addClass('animated shake');
+            $('#answer1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $(this).removeClass('animated shake');
+            });
         }
     }
 
     $scope.guess2 = '';
-    $scope.answer2 = '';
+    $scope.answer2 = 'XX';
     $scope.attempts = 0;
 
     $scope.checkAnswer2 = function() {
         if($scope.guess2.length === 0) {
-            $scope.answer2 = '';
         } else if($scope.guess2 === '602') {
-            $scope.answer2 = 'Woah, how did you get it? Either by brute force or luck?';
+            $scope.answer2 = 'CORRECT';
+            $('#answer2').css('background-color','#b8efa2');
+            $('#answer2').css('color','#75aa5f');
+            $('#answer2').css('border','1px solid #75aa5f');
             $scope.attempts++;
         } else {
-            $scope.answer2 = 'Nope, that\'s not the number I had used! Try again.';
+            $scope.answer2 = 'INCORRECT';
+            $('#answer2').css('background-color','#fc9c9c');
+            $('#answer2').css('color','#ce4848');
+            $('#answer2').css('border','1px solid #ce4848');
+            $('#answer2').addClass('animated shake');
+            $('#answer2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $(this).removeClass('animated shake');
+            });
             $scope.attempts++;
         }
     }
